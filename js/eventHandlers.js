2fac277cb4af45b863147db88427bacf8e38e40f
Ezekiel Jaramillo <jaramilloez2004@gmail.com>
2023-11-30 20:54:54 -0700

    Finish eventHandlers fr this time

--- js/eventHandlers.js
+++ js/eventHandlers.js
@@ -1,31 +1,18 @@
-let navNum = document.getElementById("navNum")
-let navPhone = document.getElementById("navPhone");
-navPhone.addEventListener("click", function() {
-    navNum.classList.toggle('active');
-}, false);
+document.getElementById("navNum").onclick = function() {
+    window.href = 'tel:8017462828';
+}
 
-let mobileCall = document.getElementById("mobileCall");
-mobileCall.addEventListener("click", function() {
-    navNumHamburger.classList.toggle('active');
-}, false);
-
-navNum.addEventListener("click", function() {
-    window.location.href = 'tel:' + 8017462828;
-}, false);
-
+document.getElementById("hamburger").onclick = function() {
+    let hamburgerMenu = document.getElementById("hamburgerMenu");
+    hamburgerMenu.classList.toggle('active');
+}
 
 document.getElementById("toTop").onclick = function() {
     window.scrollTo(0, 0);
-}
+};
 
 let resumeBtn = document.getElementById("resumeBtn");
 let resume = document.getElementById("resume")
 resumeBtn.addEventListener("click", function() {
     resume.innerHTML = `<input type=file id="resumeIn">`;
-}, false);
-
-let hamburger = document.getElementById("hamburger");
-let hamburgerMenu = document.getElementById("hamburgerMenu");
-hamburger.addEventListener("click", function() {
-    hamburgerMenu.classList.toggle('active');
 }, false);
