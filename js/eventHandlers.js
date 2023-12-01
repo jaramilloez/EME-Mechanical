dc6b86109a6f47bb89e3009316436d194cac0533
Ezekiel Jaramillo <jaramilloez2004@gmail.com>
2023-11-30 21:50:21 -0700

    Add Node.js code into server.js

--- js/eventHandlers.js
+++ js/eventHandlers.js
@@ -1,18 +1,33 @@
 document.getElementById("navNum").onclick = function() {
     window.href = 'tel:8017462828';
-}
+};
 
 document.getElementById("hamburger").onclick = function() {
     let hamburgerMenu = document.getElementById("hamburgerMenu");
     hamburgerMenu.classList.toggle('active');
-}
+};
 
 document.getElementById("toTop").onclick = function() {
     window.scrollTo(0, 0);
 };
 
-let resumeBtn = document.getElementById("resumeBtn");
-let resume = document.getElementById("resume")
-resumeBtn.addEventListener("click", function() {
+document.getElementById('resumeBtn').onclick = function() {
+    let resume = document.getElementById("resume")
     resume.innerHTML = `<input type=file id="resumeIn">`;
-}, false);
+};
+
+document.getElementById("submit").onclick = function() {
+    let inquiryForm = document.getElementById("inquiryForm");
+    let elements = inquiryForm.elements;
+    let formFilled = true;
+    for (let i = 0; i < elements.length; i++){
+        let element = elements[i];
+        if(element.required && element.value.trim() === ''){
+            formFilled = false;
+            break;
+        }
+    }
+    if(formFilled){
+        alert("Thank you");
+    }
+};
